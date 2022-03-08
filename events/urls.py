7370d89c7dd40f4720893d69fd54740a14130d26
Cameron <cameronbain@live.co.uk>
2022-03-08 21:12:38 +0000

    Added filter

--- events/urls.py
+++ events/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns= [
-    path('details/', views.details, name='events_details'),
+    path('<int:id>/', views.details, name='events_details'),
     path('', views.list, name='events_list')
 ]
